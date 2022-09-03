3913b7fb0f36b056a859f6108247c07d0571668a
ASayidov <abdullajonsayidov@gmail.com>
2022-09-03 15:58:46 +0500

    added from task 31 js part tasks 1 and 2

---
+++ 31.js
@@ -0,0 +1,38 @@
+// let array = [34, 12, 76, 87, 43]
+// let kattasi = array[0]
+// let kichigi = 0
+
+// const maxMin = () => {
+
+//   for (let i = 1; i < array.length-1; i++){
+//       if (kattasi < array[i]) {
+//         kattasi = array[i]
+//       } else if (kattasi > array[i]) { kichigi = array[i]}
+//   }
+//   let kattaKichik = kichigi.toString() +" "+ kattasi.toString()
+//   return kattaKichik
+// }
+
+// const result = maxMin()
+
+// console.log(result);
+
+
+// let raqamMatn = ['nul','bir','ikki','uch','turt','besh']
+
+// const NumberText = (suz) => {
+// let probel = suz.indexOf(" ")
+// let ajdarBoshi = suz.slice(0, probel)
+// let qolganSuz = suz.slice(1)
+// let ajdarBoshiMatn
+
+//   for (let i = 0; i < raqamMatn.length; i++) {
+//     if (ajdarBoshi==i) {ajdarBoshiMatn = raqamMatn[i]
+//     }
+//   }
+// let natija = ajdarBoshiMatn + qolganSuz
+// return natija
+// }
+
+// const result2 = NumberText("3 boshli ajdarho")
+// console.log(result2);
--- task30Abdullajon.js
+++ task30Abdullajon.js
@@ -1,27 +1,26 @@
-//task 1
-function juft (n) {
-  for (let i = 0; i < n; i += 2 ) {
-    console.log(i);
-  }
-}
+// //task 1
+// function juft (n) {
+//   for (let i = 0; i < n; i += 2 ) {
+//     console.log(i);
+//   }
+// }
 
-juft(1500)
+// juft(1500)
 
 // task 2
 
 function sonTop (n) {
-  for (let i = 1; i < n; i++ ) {
+  for (let i = 1; i < n; i += 4 ) {
     if (i%1225===0) {
       console.log("Son topildi");
     } else {console.log(i);}
   }
 }
+sonTop(1226)
 
-sonTop(2454)
-
-// task 3
-let str = ""
-for (let i = 1; i < 15; i++) {
-  str=str+i
-  console.log(str);
-}
+// // task 3
+// let str = ""
+// for (let i = 1; i < 15; i++) {
+//   str=str+i
+//   console.log(str);
+// }
