9321c2704c710311b55346ad74ae2cd5279dd55c
ASayidov <abdullajonsayidov@gmail.com>
2022-09-03 17:34:43 +0500

    ok

--- 31.js
+++ 31.js
@@ -1,41 +1,41 @@
-// let array = [34, 12, 76, 87, 43]
-// let kattasi = array[0]
-// let kichigi = 0
+let array = [34, 12, 76, 87, 43]
+let kattasi = array[0]
+let kichigi = 0
 
-// const maxMin = () => {
+const maxMin = () => {
 
-//   for (let i = 1; i < array.length-1; i++){
-//       if (kattasi < array[i]) {
-//         kattasi = array[i]
-//       } else if (kattasi > array[i]) { kichigi = array[i]}
-//   }
-//   let kattaKichik = kichigi.toString() +" "+ kattasi.toString()
-//   return kattaKichik
-// }
+  for (let i = 1; i < array.length-1; i++){
+      if (kattasi < array[i]) {
+        kattasi = array[i]
+      } else if (kattasi > array[i]) { kichigi = array[i]}
+  }
+  let kattaKichik = kichigi.toString() +" "+ kattasi.toString()
+  return kattaKichik
+}
 
-// const result = maxMin()
+const result = maxMin()
 
-// console.log(result);
+console.log(result);
 
 
-// let raqamMatn = ['nul','bir','ikki','uch','turt','besh']
+let raqamMatn = ['nul','bir','ikki','uch','turt','besh']
 
-// const NumberText = (suz) => {
-// let probel = suz.indexOf(" ")
-// let ajdarBoshi = suz.slice(0, probel)
-// let qolganSuz = suz.slice(1)
-// let ajdarBoshiMatn
+const NumberText = (suz) => {
+let probel = suz.indexOf(" ")
+let ajdarBoshi = suz.slice(0, probel)
+let qolganSuz = suz.slice(1)
+let ajdarBoshiMatn
 
-//   for (let i = 0; i < raqamMatn.length; i++) {
-//     if (ajdarBoshi==i) {ajdarBoshiMatn = raqamMatn[i]
-//     }
-//   }
-// let natija = ajdarBoshiMatn + qolganSuz
-// return natija
-// }
+  for (let i = 0; i < raqamMatn.length; i++) {
+    if (ajdarBoshi==i) {ajdarBoshiMatn = raqamMatn[i]
+    }
+  }
+let natija = ajdarBoshiMatn + qolganSuz
+return natija
+}
 
-// const result2 = NumberText("3 boshli ajdarho")
-// console.log(result2);
+const result2 = NumberText("3 boshli ajdarho")
+console.log(result2);
 
 
 let butunSonlar = [1,2,3,4,3,2,1]
