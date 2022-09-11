d062da42fc1a4617fe977f8efaae69f1bab60a62
ASayidov <abdullajonsayidov@gmail.com>
2022-09-11 09:49:23 +0500

    added task 33

---
+++ task33.js
@@ -0,0 +1,13 @@
+function drop(arr, val = 1) {
+	return arr.slice(val)
+}
+
+function incrementItems(arr) {
+	for (let i = 0; i < arr.length; i++)
+		arr[i] === arr[i] ++
+	return arr
+}
+
+function findIndex(arr, str) {
+	return arr.indexOf(str)
+}
